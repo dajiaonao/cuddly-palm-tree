f63dcbfb358db67f68fd13e823d6cfa4a3dff220
dajiaonao <zdl.linqi@gmail.com>
2018-07-04 15:43:05 +0800

    Update eagle.py

--- eagle.py
+++ eagle.py
@@ -1,6 +1,8 @@
 from subprocess import call
 from PyDE import DE
-import socket
+#import socket
+from socket import *
+import time
 
 def move(actionList):
     '''call the function to move to the right location'''
@@ -13,7 +15,8 @@ def getL():
     '''Get the strength of the light from TCP/IP'''
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     port = 1024
-    s.connect((self.host,port))
+    host = '192.168.1.10'
+    s.connect((host,port))
 
     cmdstr = 'aa'
     s.sendall(cmdstr)
@@ -21,7 +24,41 @@ def getL():
 
     L = int(L_str)
     return L
+def getNx():
+    host = '192.168.1.10'
+    host_rx = ''
+    port = 13141
+    addr = (host,port)
+    addr_rx = (host_rx,port)
+    udpClient = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    udpServer = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    print "A"
+    udpServer.bind(addr_rx)
+    print "B"
+    udpClient.sendto('check',addr)
+    print "C"
+    bufsize = 1024
+    data,addr_r = udpServer.recvfrom(bufsize)
+    print(data)
 
+def getN():
+    host = "192.168.1.10"
+    host_rx = ''
+    port = 13141
+    addr = (host,port)
+    print "A"
+    addr_rx = (host_rx,port)
+    udpClient = socket(AF_INET,SOCK_DGRAM)
+    print "B"
+    udpServer = socket(AF_INET,SOCK_DGRAM)
+    print "C"
+    udpServer.bind(addr_rx)
+    print "D"
+    udpClient.sendto("check",addr)
+    print "F"
+    bufsize = 1024
+    data,addr_r = udpServer.recvfrom(bufsize)
+    print(data)
 
 def funX1(x):
     # move to location X
@@ -47,4 +84,6 @@ def test1():
 if __name__ == '__main__':
     print "testing"
     #test1()
-    run2()
+    #run2()
+    getN()
+    #getL()
